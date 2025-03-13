4a4924ec499c36902443ca6eb125e39d6de226c1
animalelder <wllearned@icloud.com>
2025-03-13 15:32:42 -0700

    Changed to Next@15.2, adjusted route handler for auth, auth client, and added middleware

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,6 +1,8 @@
 // src/lib/auth-client.ts
 import { createAuthClient } from 'better-auth/react';
 
-export const authClient = createAuthClient({});
+export const authClient = createAuthClient({
+  baseURL: process.env.BETTER_AUTH_URL,
+});
 
 export const { signIn, signOut, signUp, useSession } = authClient;
