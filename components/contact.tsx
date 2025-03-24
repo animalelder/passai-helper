e540061284fd2107896c31d08ec6318573f641ad
animalelder <wllearned@icloud.com>
2025-03-24 16:58:16 -0700

    FIX: unescaped characters in JSX code in contact.jsx

--- components/contact.tsx
+++ components/contact.tsx
@@ -19,13 +19,13 @@ export default function ContactSection() {
           Contact Sales
         </h1>
         <p className="mt-4 text-center">
-          We'll help you find the right plan and pricing for your business.
+          We&rsquo;ll help you find the right plan and pricing for your business.
         </p>
 
         <Card className="mx-auto mt-12 max-w-lg p-8 shadow-md sm:p-16">
           <div>
             <h2 className="text-xl font-semibold">
-              Let's get you to the right place
+              Let&rsquo;s get you to the right place
             </h2>
             <p className="mt-4 text-sm">
               Reach out to our sales team! We’re eager to learn more about how you
@@ -76,7 +76,7 @@ export default function ContactSection() {
                 id="website"
               />
               <span className="text-muted-foreground inline-block text-sm">
-                Must start with 'https'
+                Must start with &rsquo;https&rsquo;
               </span>
             </div>
 
