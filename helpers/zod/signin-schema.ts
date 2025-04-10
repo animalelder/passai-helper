b731be4ce087beca2a7daefa81085276a27ffe78
animalelder <wllearned@icloud.com>
2025-04-10 13:03:18 -0700

    FEAT auth: Update validation schemas for sign in and sign up to check for passwords between 8-20 characters with 1 number 1 uppercase letter and 1 special character

--- helpers/zod/signin-schema.ts
+++ helpers/zod/signin-schema.ts
@@ -15,7 +15,7 @@ const SigninSchema = z.object({
   email: z
     .string()
     .email({ message: "Invalid email" })
-    .min(1, { message: "Email is required" }),
+    .min(6, { message: "Email is required" }),
   password: z
     .string()
     .min(8, { message: "Password must be at least 8 characters long" })
--- helpers/zod/signup-schema.ts
+++ helpers/zod/signup-schema.ts
@@ -15,13 +15,17 @@ export const SignupSchema = z.object({
   name: z
     .string()
     .min(2, { message: "Minimum 2 characters are required" })
-    .max(20, { message: "Maximum of 20 characters are allowed" }),
+    .max(25, { message: "Maximum of 25 characters are allowed" })
+    .regex(/^[a-zA-Z ]*$/, { message: "Only letters are allowed" }),
   email: z
     .string()
     .email({ message: "Invalid email address" })
-    .min(1, { message: "Email is required" }),
+    .min(6, { message: "Email is required" }),
   password: z
     .string()
-    .min(8, { message: "Password must be at least 6 characters long" })
-    .max(20, { message: "Password must be at most 20 characters long" }),
+    .min(8, { message: "Password must be at least 8 characters long" })
+    .max(20, { message: "Password must be at most 20 characters long" })
+    .regex(/[!@#$%^&*(),.?":{}|<>]/, "Password must include a special character")
+    .regex(/[A-Z]/, "Password must include an uppercase letter")
+    .regex(/\d/, "Password must include a number"),
 });
