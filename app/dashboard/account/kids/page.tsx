a765e6c9ee95f816f82c46134db9cccf8cb75dbc
animalelder <wllearned@icloud.com>
2025-04-30 14:10:13 -0700

    Added subroutes and pages for dashboard

---
+++ app/dashboard/account/kids/page.tsx
@@ -0,0 +1,7 @@
+export default async function Page() {
+  return (
+    <div>
+      <h1>Account Page for Kids</h1>
+    </div>
+  );
+}
---
+++ app/dashboard/settings/page.tsx
@@ -0,0 +1,7 @@
+export default async function Page() {
+  return (
+    <div>
+      <h1>Settings Page</h1>
+    </div>
+  );
+}
