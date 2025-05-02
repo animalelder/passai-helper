3c6b9de7e8bd8eead3c8b04ea8d75698dcddf33b
animalelder <wllearned@icloud.com>
2025-05-01 17:46:46 -0700

    TEMP FIX: removed the auth from the dashboard layout. It can go in other places.

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,9 +1,6 @@
 // src/app/dashboard/layout.tsx
 import React from "react";
-import { headers } from "next/headers";
 
-import { auth } from "@/lib/auth";
-import type { Session } from "@/lib/auth-types";
 import Sidebar from "@/components/dashboard/sidebar";
 import TopHeaderBar from "@/components/dashboard/top-header-bar";
 
@@ -12,17 +9,6 @@ export default async function DashboardLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const session: Session | null = await auth.api.getSession({
-    headers: await headers(),
-  });
-
-  // Disabling session check during development
-  if (!session) {
-    // eslint-disable-next-line no-console
-    console.log("No session found");
-    // throw redirect("/signin");
-  }
-
   return (
     <div className="flex min-h-screen w-screen bg-gradient-to-br from-amber-50 to-emerald-700">
       {/* Sidebar fixed width */}
@@ -32,7 +18,7 @@ export default async function DashboardLayout({
 
       <div className="flex flex-1 flex-col bg-white">
         {/* Top Header Bar */}
-        <TopHeaderBar userName={session?.user.name || "Ashley"} />
+        <TopHeaderBar userName={"Ashley"} />
 
         <main className="flex-1">{children}</main>
       </div>
